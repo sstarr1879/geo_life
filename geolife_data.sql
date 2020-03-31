16e2503d3925c426184c18803fdf28e686b1ac00
Sarah <sstarr1879@gmail.com>
2020-03-31 12:25:35 -0400

    update sql with postgis spatial index

--- geolife_data.sql
+++ geolife_data.sql
@@ -1,16 +1,26 @@
 -- Table: public.geolife_data
 
-DROP TABLE public.geolife_data;
+-- DROP TABLE public.geolife_data;
 
 CREATE TABLE public.geolife_data
 (
-  objectid integer NOT NULL,
-  active_date timestamp,
+  objectid integer,
+  active_date timestamp without time zone,
   lat numeric(38,8),
   lon numeric(38,8),
   alt numeric(38,2),
   label_id character varying(200),
   user_id character varying(200),
-  geometry point,
-  CONSTRAINT geolife_data_pkey PRIMARY KEY (objectid)
+  uuid integer NOT NULL DEFAULT nextval('geolife_data_uuid_seq'::regclass),
+  geom geometry(Point,4326),
+  CONSTRAINT geolife_data_pkey PRIMARY KEY (uuid)
 )
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.geolife_data
+  OWNER TO postgres;
+
+-- CREATE INDEX [indexname] ON [tablename] USING GIST ( [geometrycolumn] );
+
+CREATE INDEX geolife_data_gix ON geolife_data USING GIST (geom);
